f9ddc5faba0d7b6f993833995fa5fe291cdd9e8e
leylakhalilli <leylaxelilli65@gmail.com>
2022-07-25 19:29:33 +0400

    created table

--- src/main/java/com/example/registrationlogin/dao/entity/Role.java
+++ src/main/java/com/example/registrationlogin/dao/entity/Role.java
@@ -1,4 +1,22 @@
-package com.example.registrationlogin.dao.entity;
-
-public class Role {
-}
+//package com.example.registrationlogin.dao.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Table(name = "role")
+//public class Role {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String roleName;
+//
+//}
--- src/main/java/com/example/registrationlogin/dao/entity/User.java
+++ src/main/java/com/example/registrationlogin/dao/entity/User.java
@@ -1,4 +1,25 @@
-package com.example.registrationlogin.dao.entity;
-
-public class User {
-}
+//package com.example.registrationlogin.dao.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Table(name = "user")
+//public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String firstName;
+//    private String lastName;
+//    private String email;
+//    private String password;
+//    private String roles;
+//}
