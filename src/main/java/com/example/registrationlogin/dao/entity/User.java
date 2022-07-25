16e66b3062692b051ef3ad91d9c6d96afe1ea61a
leylakhalilli <leylaxelilli65@gmail.com>
2022-07-25 19:30:55 +0400

    added user entity

--- src/main/java/com/example/registrationlogin/dao/entity/User.java
+++ src/main/java/com/example/registrationlogin/dao/entity/User.java
@@ -1,25 +1,25 @@
-//package com.example.registrationlogin.dao.entity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name = "user")
-//public class User {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String firstName;
-//    private String lastName;
-//    private String email;
-//    private String password;
-//    private String roles;
-//}
+package com.example.registrationlogin.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String roles;
+}
