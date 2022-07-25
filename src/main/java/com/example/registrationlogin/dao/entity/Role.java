18efbad91892482ce54750af069d1556ffae4157
leylakhalilli <leylaxelilli65@gmail.com>
2022-07-25 19:32:01 +0400

    added role entity

--- src/main/java/com/example/registrationlogin/dao/entity/Role.java
+++ src/main/java/com/example/registrationlogin/dao/entity/Role.java
@@ -1,22 +1,22 @@
-//package com.example.registrationlogin.dao.entity;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name = "role")
-//public class Role {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String roleName;
-//
-//}
+package com.example.registrationlogin.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+}
